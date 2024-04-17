cbaeaa1133d13ed3a46d598c78e88f358135293e
Ricardo Salles <salles.ricardo@gmail.com.br>
2024-04-16 23:47:12 -0300

    Vars, user input, formatted printout

--- basic/main.go
+++ basic/main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello World!")
+    var value1 int = 1
+    fmt.Println("Hello World!", value1)
+    var input string
+    fmt.Println("Type something:")
+    fmt.Scan(&input)
+    fmt.Printf("You typed: \"%v\"", input)
 }
