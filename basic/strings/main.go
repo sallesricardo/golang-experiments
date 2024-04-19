be6377aa17898853000ff742b568d73b3e4ad797
Ricardo Salles <salles.ricardo@gmail.com.br>
2024-04-19 00:00:38 -0300

    Tests with strings

---
+++ basic/strings/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+    var myString = []rune("résumé")
+    var indexed = myString[1]
+    fmt.Printf("%v, %T\n", indexed, indexed)
+
+    for i, v := range myString {
+        fmt.Println(i, v)
+    }
+
+    fmt.Printf("\nThe length of 'myString' is %v\n", len(myString))
+
+    var myRune = 'a'
+    fmt.Printf("\nmyRune = %v", myRune)
+
+    var strSlice = []string{"s", "u", "b", "s", "c", "r", "i", "b", "e"}
+    var catStr = ""
+    var strBuilder strings.Builder
+
+    for i := range strSlice {
+        catStr += strSlice[i]
+        strBuilder.WriteString(strSlice[i])
+    }
+    fmt.Printf("\n%v\n", catStr)
+    fmt.Printf("%v\n", strBuilder.String())
+}
